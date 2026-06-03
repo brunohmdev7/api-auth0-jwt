be4c1338ebc696f158157b256f09ae3ac0af42fa
brunohmdev <brunohm.dev@gmail.com>
2026-06-02 22:41:38 -0300

    subindo validação de token

--- src/main/java/dev/brunohm/apiauth0jwt/security/SecurityFilter.java
+++ src/main/java/dev/brunohm/apiauth0jwt/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package dev.brunohm.apiauth0jwt.security;
 
+import dev.brunohm.apiauth0jwt.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,9 +12,17 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = recuperarToken(request);
+        var subject = tokenService.getSubject(token);
 
         filterChain.doFilter(request, response);
     }
@@ -21,7 +30,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            return authorizationHeader.substring(7);
+            return authorizationHeader.substring(7); // Remove o "Bearer "
         } else {
             throw new RuntimeException("Token de autenticação ausente ou inválido");
         }
--- src/main/java/dev/brunohm/apiauth0jwt/service/TokenService.java
+++ src/main/java/dev/brunohm/apiauth0jwt/service/TokenService.java
@@ -3,6 +3,7 @@ package dev.brunohm.apiauth0jwt.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import dev.brunohm.apiauth0jwt.entity.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,21 @@ public class TokenService {
                 .withSubject(usuario.getUsername())
                 .withExpiresAt(dataExpiracao())
                 .sign(algorithm);
+    }
+
+    public String getSubject(String token) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("api-auth0-jwt")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado");
+        }
     }
 
     private Instant dataExpiracao() {
