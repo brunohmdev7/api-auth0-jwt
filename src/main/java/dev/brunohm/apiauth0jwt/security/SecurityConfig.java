c9ec553ab5d77e9c87f70b1c0b5caae5df2f4134
brunohmdev <brunohm.dev@gmail.com>
2026-06-03 20:03:59 -0300

    subindo filtro de segurança

--- src/main/java/dev/brunohm/apiauth0jwt/security/SecurityConfig.java
+++ src/main/java/dev/brunohm/apiauth0jwt/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
--- src/main/java/dev/brunohm/apiauth0jwt/security/SecurityFilter.java
+++ src/main/java/dev/brunohm/apiauth0jwt/security/SecurityFilter.java
@@ -42,9 +42,8 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            return authorizationHeader.substring(7); // Remove o "Bearer "
-        } else {
-            throw new RuntimeException("Token de autenticação ausente ou inválido");
+            return authorizationHeader.substring(7);
         }
+        return null;
     }
 }
